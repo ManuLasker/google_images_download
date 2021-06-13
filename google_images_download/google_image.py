e65fbd8e15fe621915cacc378763c146be797b0f
ManuLasker <emanuel.afanador@koombea.com>
2021-06-13 13:38:39 -0500

    Implement scroll down to continue scrapping src img property

--- google_images_download/google_image.py
+++ google_images_download/google_image.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from config import ImgTagHasSrc
+from validators import ImageSrcBase64, ImageSrcUrl
 
-from typing import List
+from typing import List, Tuple
 from pathlib import Path
 from config import get_chrome_options
 from logger import log_info
@@ -24,13 +25,10 @@ class GoogleImage:
         self.scroll_to_element = "arguments[0].scrollIntoView(true);"
         self.max_scroll = max_scroll
 
-    def get_image_links(self) -> List[str]:
+    def get_image_links(self) -> Tuple[List[ImageSrcUrl], List[ImageSrcBase64]]:
         img_results: List[WebElement] = self.driver.find_elements_by_xpath(self.xpath)
-        first_1 = img_results[0]
-        # print quantity of images
-        log_info(f"Lenght of list of results = {len(img_results)}")
-        # Create urls list for image searched
-        image_urls = []
+        # Create urls, base64  for image searched
+        image_urls, images_base64 = [], []
         # loop through images results
         index, max_scroll = 0, self.max_scroll
         while index < len(img_results) and max_scroll != 0:
@@ -38,13 +36,17 @@ class GoogleImage:
                 img_src: str = WebDriverWait(img_results[index], 10).until(
                     ImgTagHasSrc()
                 )
-                log_info(img_src)
-                image_urls.append(img_src)
+                try:
+                    temp = ImageSrcUrl(url=img_src)
+                    if not temp in image_urls:
+                        image_urls.append(temp)
+                except:
+                    temp = ImageSrcBase64(base64_src=img_src)
+                    if not temp in images_base64:
+                        images_base64.append(temp)
             except Exception as error:
                 log_info(f"There was an error getting image source {error}")
-
             index += 1
-
             if index == len(img_results):
                 self.driver.execute_script(
                     self.scroll_to_element, img_results[index - 1]
@@ -56,9 +58,10 @@ class GoogleImage:
                 index = 0
                 max_scroll -= 1
                 log_info(
-                    f"Doing a scroll down, new result list lenght = {len(img_results)}"
+                    f"Doing a scroll down, Images scrapped:"
+                    f" = {len(image_urls) + len(images_base64)}"
                 )
-        return image_urls
+        return image_urls, images_base64
 
 
 class Google:
@@ -66,10 +69,14 @@ class Google:
         self.driver_path = driver_path
         self.url = "https://www.google.com/search?q={}"
 
-    def search(self, query_search: str, max_scroll: int = 3) -> List[str]:
+    def search(
+        self, query_search: str, max_scroll: int = 3
+    ) -> Tuple[List[ImageSrcUrl], List[ImageSrcBase64]]:
         with Chrome(self.driver_path) as driver:
             # search in google
             driver.get(self.url.format(query_search))
+            # wait
+            time.sleep(0.5)
             # search in image section
             google_image = GoogleImage(driver, max_scroll)
             return google_image.get_image_links()
