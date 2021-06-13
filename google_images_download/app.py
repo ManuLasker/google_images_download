7ace5250821e9e7ab3616d821b4648f669be5ae1
ManuLasker <emanuel.afanador@koombea.com>
2021-06-13 13:37:55 -0500

    Finish implementation of scrapping, and downloading each image_name

--- google_images_download/app.py
+++ google_images_download/app.py
@@ -7,6 +7,7 @@ from parameters_callback import (
     image_directory_callback,
     chromedriver_directory_callback,
 )
+import validators, files_callback, image_download
 
 
 def main(
@@ -45,6 +46,14 @@ def main(
     # search just one
     for image_name in image_names:
         google = Google(chromedriver_directory / "chromedriver")
-        images_link = google.search(image_name)
-    # finish scrapping src of img tag in google image search!
-    # TODO: Save base64 images source, download image url and save it!
+        # scrap image from google search egine
+        images_link, images_base64 = google.search(image_name)
+        # save image to disk
+        # url save
+        image_name = "_".join(image_name.strip().split())
+        log_info("saving images to {}".format(image_directory / image_name))
+        url_save = image_download.ImageSrcUrlDownload(image_directory, image_name)
+        url_save(images_link)
+        # base64 save
+        base64_save = image_download.ImageSrcBase64Download(image_directory, image_name)
+        base64_save(images_base64)
