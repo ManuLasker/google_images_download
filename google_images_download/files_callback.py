b3bcfdb1de8b068b735a39dd0ca8ee6f530a671e
ManuLasker <emanuel.afanador@koombea.com>
2021-06-13 13:39:08 -0500

    Implement file utility functions to save image from base64 and binary image

--- google_images_download/files_callback.py
+++ google_images_download/files_callback.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from logger import raise_error, log_info
 from google_image import Google
+import uuid
+from base64 import b64decode
 
 
 def create_dir(directory_path: Path, verbose: bool = False) -> Path:
@@ -22,3 +24,14 @@ def check_chromedriver_file(chromedriver_file: Path) -> None:
         google.check()
     except Exception as error:
         raise error
+
+
+def save_bytes_image(bytes_image: bytes, directory_path: Path, image_name: str):
+    with open(
+        directory_path / "{}_{}.jpg".format(image_name, uuid.uuid4().hex), "wb"
+    ) as file_image:
+        file_image.write(bytes_image)
+
+
+def save_base64_image(base64_image: str, directory_path: Path, image_name: str):
+    save_bytes_image(b64decode(base64_image), directory_path, image_name)
